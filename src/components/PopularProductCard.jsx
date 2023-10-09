3b8ed542ed627838de71d5908edd2cf8008c00b2
Samuel Aghoghovbia Jr <lilsamoaj@yahoo.com>
2023-10-08 19:53:59 -0500

    Fixed rating number alignment

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -10,14 +10,14 @@ const PopularProductCard = ({imgURL, name, price}) => {
             className='w-[280px] h-[280px]'
             />
         <div className='mt-8 flex
-        justify-start gap-2.5 border'>
+        justify-start gap-2.5'>
             <img 
                 src={star} alt="rating"
                 width={24} height={24}
             />
             <p className='font-montserrat
             text-xl leading-normal
-            text-slate-gray border'>(4.5)</p>
+            text-slate-gray mt-7 md:mt-12'>(4.5)</p>
             <div>
                 <h3 className='mt-2 text-2xl
                 leading-normal font-semibold
